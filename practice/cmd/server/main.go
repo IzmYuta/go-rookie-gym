8941feabccdae000533cfd5c8a114edd1e3da611
IzmYuta <y.izumi@4nonome.com>
2023-10-24 21:50:23 +0900

    Connect DB

--- practice/cmd/server/main.go
+++ practice/cmd/server/main.go
@@ -4,12 +4,50 @@ import (
 	"fmt"
 	"log"
 	"io"
+	"context"
 	"encoding/json"
 	"net/http"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct{
+	ID int
+	Name string
+}
 
 func main() {
+	db, err := sql.Open("mysql", "user:password@tcp(localhost:3306)/database?charset=utf8&parseTime=true")
+	if err != nil {
+		log.Printf("failed to open a db err = %s", err.Error())
+		return
+	}
+
+	if err := db.PingContext(context.Background()); err != nil {
+		log.Printf("failed to ping err = %s", err.Error())
+		return
+	}
+	id, err := db.ExecContext(context.Background(), "INSERT INTO users (name) VALUES (?);", "sample User")
+	if err != nil {
+		log.Printf("failed to exec query err = %s", err.Error())
+		return
+	}
+
+	lastid, err := id.LastInsertId()
+	if err != nil {
+		log.Printf("failed to get a last insert id err = %s", err.Error())
+		return
+	}
+	var s User
+
+	if err := db.QueryRowContext(context.Background(), "SELECT id, name FROM users WHERE id = ?", lastid).Scan(&s.ID, &s.Name); err != nil {
+		log.Printf("failed to scan err = %s", err.Error())
+		return
+	}
+
+	log.Printf("User is %#v", s)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
